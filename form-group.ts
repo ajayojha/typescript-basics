91ca7f586559b2444ca5f6a90e7e973ad8341ac8
ajayojha <ojhaajay@outlook.com>
2020-06-15 07:25:49 +0530

    previous

--- form-group.ts
+++ form-group.ts
@@ -11,23 +11,3 @@ let formGroup = this.formBuilder.group({
         })
     ])
 })
-
-export interface Address {
-    countryName: string;
-}
-
-export interface Skill {
-    name: string;
-}
-
-export class User {
-
-    firstName: string;
-
-    dob: Date;
-
-    address: Address;
-
-    skills: Skill[]
-
-}
