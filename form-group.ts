c26c53f3f0b49c45e3e0d1a2758553a8896ce696
ajayojha <ojhaajay@outlook.com>
2020-06-15 07:43:01 +0530

    change

--- form-group.ts
+++ form-group.ts
@@ -1,13 +1,13 @@
 let data: any = { firstName: 'John', address: { countryName: "India" }, dob: "10/15/2020", skills: [{ name: 'Angular' }] }
 
-let formGroup = this.formBuilder.group({
+let formGroup = this.formBuilder.group<User>({
     firstName: [serverData.firstName, Validators.required, Validators.maxLength(10)],
     dob: [new Date(serverData.dob), Validators.required],
-    address: this.formBuilder.group({
+    address: this.formBuilder.group<Address>({
         countryName: [serverData.countryName, Validators.required]
     }),
-    skills: this.formBuilder.array([
-        this.formBuilder.group({
+    skills: this.formBuilder.array<Skill[]>([
+        this.formBuilder.group<Skill>({
             name: [serverData.skills[0].name, Validators.required]
         })
     ])
