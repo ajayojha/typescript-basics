a4ec5627d1105bc31619c743d565bffeebb626fb
ajayojha <ojhaajay@outlook.com>
2020-06-15 10:50:32 +0530

    changes

--- tscode.ts
+++ tscode.ts
@@ -1,1 +1,4 @@
-user.address.countryName = "United States"
+let formArray = user.controls.skills as FormArray<Skill[]>;
+formArray.push(this.formBuilder.group({
+    name: ['', Validators.required]
+}))
