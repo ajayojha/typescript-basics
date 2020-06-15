e96e5d4b049f5394196d4ca6ec48dd5b11e6a371
ajayojha <ojhaajay@outlook.com>
2020-06-15 07:27:21 +0530

    changed

--- form-group.ts
+++ form-group.ts
@@ -1,12 +1,31 @@
-let data: any = {firstName: 'John',address: { countryName: "India" },dob: "10/15/2020",skills: [{ name: 'Angular' }]} 
-let formGroup = this.formBuilder.group({
+export interface Address {
+    countryName: string;
+}
+
+export interface Skill {
+    name: string;
+}
+
+export interface User {
+
+    firstName: string;
+
+    dob: Date;
+
+    address: Address;
+
+    skills: Skill[]
+
+}
+let data: any = { firstName: 'John', address: { countryName: "India" }, dob: "10/15/2020", skills: [{ name: 'Angular' }] } 
+let formGroup = this.formBuilder.group<User>({
     firstName: [serverData.firstName, Validators.required, Validators.maxLength(10)],
     dob: [new Date(serverData.dob), Validators.required],
-    address: this.formBuilder.group({
+    address: this.formBuilder.group<Address>({
         countryName: [serverData.countryName, Validators.required]
     }),
-    skills: this.formBuilder.array([
-        this.formBuilder.group({
+    skills: this.formBuilder.array<Skills[]>([
+        this.formBuilder.group<Skill>({
             name: [serverData.skills[0].name, Validators.required]
         })
     ])
