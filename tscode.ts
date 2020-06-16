3e1a3b05748922389d3d7c6c0c540dc0786b2c52
ajayojha <ojhaajay@outlook.com>
2020-06-16 11:11:26 +0530

    changes

--- tscode.ts
+++ tscode.ts
@@ -1,12 +1,2 @@
-let user = this.formBuilder.group({
-    firstName: [serverData.firstName, Validators.required, Validators.maxLength(10)],
-    dob: [new Date(serverData.dob), Validators.required],
-    address: this.formBuilder.group({
-        countryName: [serverData.countryName, Validators.required]
-    }),
-    skills: this.formBuilder.array([
-        this.formBuilder.group({
-            name: [serverData.skills[0].name, Validators.required]
-        })
-    ])
-})
+let user = (this.formBuilder.group(User,
+                <Options>{ data: this.serverData }) as IFormGroup<User>).modelInstance;
