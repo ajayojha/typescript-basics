b8c6bc8c60c35af016b00aa5f9c193ebada44752
ajayojha <ojhaajay@outlook.com>
2020-06-16 11:10:06 +0530

    changes

--- tscode.ts
+++ tscode.ts
@@ -1,1 +1,12 @@
-user.skills.push(new Skill());
+let user = this.formBuilder.group({
+    firstName: [serverData.firstName, Validators.required, Validators.maxLength(10)],
+    dob: [new Date(serverData.dob), Validators.required],
+    address: this.formBuilder.group({
+        countryName: [serverData.countryName, Validators.required]
+    }),
+    skills: this.formBuilder.array([
+        this.formBuilder.group({
+            name: [serverData.skills[0].name, Validators.required]
+        })
+    ])
+})
