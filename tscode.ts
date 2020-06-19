600dac29cd0cb6c29b726e34646f7e93d13e70e0
ajayojha <ojhaajay@outlook.com>
2020-06-19 05:52:24 +0530

    change

--- tscode.ts
+++ tscode.ts
@@ -1,2 +1,32 @@
-let user = (this.formBuilder.group(User,
-                <Options>{ data: this.serverData }) as IFormGroup<User>).modelInstance;
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, } from "@angular/forms"
+import { IFormBuilder, IFormGroup } from "@rxweb/types"
+import { User } from './user'; import { Address } from './address'; import { Skill } from './skill';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    formGroup: FormGroup;
+    formBuilder: FormBuilder;
+
+    constructor(formBuilder: FormBuilder) {
+        this.formBuilder = formBuilder;
+    }
+
+    ngOnInit() {
+        this.formGroup = this.formBuilder.group({
+            firstName: ['', [Validators.required]],
+            address: this.formBuilder.group({
+                countryName: ["", Validators.required]
+            }),
+            skills: this.formBuilder.array([
+                this.formBuilder.group({
+                    name: ["", Validators.required]
+                })
+            ])
+        });
+    }
+}
