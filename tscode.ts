e7d18390d903de3f8368b8ba95b19ec5c7820c73
ajayojha <ojhaajay@outlook.com>
2020-06-19 05:58:51 +0530

    change

---
+++ tscode.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, } from "@angular/forms"
+import { IFormBuilder, IFormGroup } from "@rxweb/types"
+import { User } from './user'; import { Address } from './address'; import { Skill } from './skill';
+
+@Component({...})
+export class UserComponent implements OnInit {
+    formGroup: FormGroup;
+    formBuilder: FormBuilder;
+
+    constructor(formBuilder: FormBuilder) {
+        this.formBuilder = formBuilder;
+    }
+
+    ngOnInit() {
+        this.formGroup = this.formBuilder.group({
+            firstName: ['', [Validators.required]],
+            address: this.formBuilder.group({
+                countryName: ["", Validators.required]
+            }),
+            skills: this.formBuilder.array([
+                this.formBuilder.group({
+                    name: ["", Validators.required]
+                })
+            ])
+        });
+    }
+}
