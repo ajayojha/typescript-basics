8800ec199b3d3276c8d99e0ff0108f2371a1df79
ajayojha <ojhaajay@outlook.com>
2020-06-19 06:28:36 +0530

    changes

--- tscode.ts
+++ tscode.ts
@@ -1,10 +1,3 @@
-import { Address } from './address';
-import { Skill } from './skill';
-
-export interface User {
-    firstName: string;
-
-    address: Address;
-
-    skills: Skill[];
+export interface Skill {
+    name: string;
 }
