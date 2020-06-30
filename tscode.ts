58c3226a836f125f8303616b6ae6011d6e7ed1e5
ajayojha <ojhaajay@outlook.com>
2020-06-30 08:06:32 +0530

    change

--- tscode.ts
+++ tscode.ts
@@ -1,5 +1,1 @@
-let formArray: IFormArray<Skill> = new FormArray([
-    new FormGroup(<ControlProp<Skill>>{
-        name: new FormControl((<ControlState<string>>""))
-    }) as IFormGroup<Skill>
-])
+let formControl: FormControl = new FormControl("");
