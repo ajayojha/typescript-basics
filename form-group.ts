93d84cd48bc1a241f438392f37712e2fecf6ade5
Ajay Ojha <ojhaajay@outlook.com>
2020-09-11 20:32:54 +0530

    Update form-group.ts

--- form-group.ts
+++ form-group.ts
@@ -1,1 +1,1 @@
-let data: any = { firstName: 'John', address: { countryName: "India" }, dob: "10/15/2020", skills: [{ name: 'Angular' }] 
+let test: any = { firstName: 'John', address: { countryName: "India" }, dob: "10/15/2020", skills: [{ name: 'Angular' }] 
