e0a52330822a3ca7de530d1cc7b360dae014eec3
Ajay Ojha <ojhaajay@outlook.com>
2020-11-19 22:52:20 +0530

    changes

--- interface.ts
+++ interface.ts
@@ -7,15 +7,3 @@ export class Skill {
     @required()
     name: string;
 }
-
-export class User {
-    @requried()
-    @maxLength(10)
-    firstName: string;
-    @toDate()
-    dob: Date;
-    @propObject(Address)
-    address: Address;
-    @propArray(Skill)
-    skills: Skill[]
-}
